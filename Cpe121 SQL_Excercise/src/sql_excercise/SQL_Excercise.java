7e71036b2458cdecfd00c736839318213fed876e
DotYohan <yuanbrioness@gmail.com>
2025-03-28 17:47:17 +0800

    Add files via upload

---
+++ Cpe121 SQL_Excercise/src/sql_excercise/SQL_Excercise.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sql_excercise;
+
+import java.lang.System.Logger;
+import java.lang.System.Logger.Level;
+import java.sql.*;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author yuanb
+ */
+public class SQL_Excercise {
+
+
+    public static void main(String[] args) {
+        SaveData save = new SaveData();
+        save.setVisible(true);
+    }
+
+  
+    
+}
